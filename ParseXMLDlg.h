1458889a14f0f9cb47cc2383702c970389c33cd8
precode <flyzsf@163.com>
2016-04-23 18:46:32 +0800

    testcommit

---
+++ ParseXMLDlg.h
@@ -0,0 +1,43 @@
+
+// ParseXMLDlg.h : 头文件
+//
+
+#pragma once
+#include "afxwin.h"
+#include "rapidxml\rapidxml.hpp"
+#include "rapidxml\rapidxml_iterators.hpp"
+#include "rapidxml\rapidxml_print.hpp"
+#include "rapidxml\rapidxml_utils.hpp"
+
+using namespace rapidxml;
+
+// CParseXMLDlg 对话框
+class CParseXMLDlg : public CDialogEx
+{
+// 构造
+public:
+	CParseXMLDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_PARSEXML_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedBtnCreatexml();
+	void ReadXML(int &iEquipment,int &iGpibAddr);
+	CComboBox m_combo_Equipment;
+	CComboBox m_Combo_GpibAddr;
+};
